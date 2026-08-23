0845c13b7ca6e8785945a8d9b8896a3f2b2913b4
zathiz <zathiz@gmail.com>
2026-08-23 19:32:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector(".menu-toggle");
-const links = document.querySelector(".nav-links");
-if (menu) menu.addEventListener("click", () => links.classList.toggle("open"));
-document.querySelectorAll(".nav-links a").forEach(a => a.addEventListener("click", () => links.classList.remove("open")));
+const menu=document.querySelector('.menu-toggle');
+const links=document.querySelector('.nav-links');
+if(menu) menu.addEventListener('click',()=>links.classList.toggle('open'));
+document.querySelectorAll('.nav-links a').forEach(a=>a.addEventListener('click',()=>links.classList.remove('open')));
